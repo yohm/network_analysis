0d2ad06f6400701e7254e815923f0353c9e2e834
Yohsuke Murase <yohsuke.murase@gmail.com>
2016-08-01 17:06:14 +0300

    bug fix in plotting script

--- plot/plot_all.sh
+++ plot/plot_all.sh
@@ -4,5 +4,6 @@ set -eux
 INPUT_DIR=$1
 OUTPUT_DIR=$INPUT_DIR
 mkdir -p $OUTPUT_DIR
-find . -name "*.plt" | xargs -n 1 -t gnuplot -e "indir='$INPUT_DIR'; outdir='$OUTPUT_DIR'"
+script_dir=$(cd $(dirname $BASH_SOURCE); pwd)
+find $script_dir -name "*.plt" | xargs -n 1 -t gnuplot -e "indir='$INPUT_DIR'; outdir='$OUTPUT_DIR'"
 
