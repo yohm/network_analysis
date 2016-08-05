4cd217482fd6cc82e77ebb6032f93e395dd5fc58
Yohsuke Murase <yohsuke.murase@gmail.com>
2016-08-05 16:15:15 +0300

    changed the default figure directory to the current directory

--- plot/plot_all.sh
+++ plot/plot_all.sh
@@ -2,7 +2,7 @@
 set -eux
 
 INPUT_DIR=$1
-OUTPUT_DIR=${2:-$INPUT_DIR}
+OUTPUT_DIR=${2:-.}
 mkdir -p $OUTPUT_DIR
 script_dir=$(cd $(dirname $BASH_SOURCE); pwd)
 find $script_dir -name "*.plt" | xargs -n 1 -t gnuplot -e "indir='$INPUT_DIR'; outdir='$OUTPUT_DIR'"
