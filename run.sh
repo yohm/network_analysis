273e1fcd3d2137babd67c7ce758a83b977b6ed01
Yohsuke Murase <yohsuke.murase@gmail.com>
2016-08-05 12:13:15 +0300

    make the second arguments optional

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 set -eux
 
 NET_FILE=$(cd $(dirname $1) && pwd)/$(basename $1)
-OUT_DIR=$2
+OUT_DIR=${2:-.}  # default output dir is the current directory
 script_dir=$(cd $(dirname $BASH_SOURCE); pwd)
 
 mkdir -p $OUT_DIR
