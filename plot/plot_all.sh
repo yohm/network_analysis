03a5320dd72f27bdec946d3beb82928dd39c1477
Yohsuke Murase <yohsuke.murase@gmail.com>
2022-08-15 05:12:44 +0300

    call pipenv

--- plot/plot_all.sh
+++ plot/plot_all.sh
@@ -5,6 +5,7 @@ INDIR=$(cd $1 && pwd)
 OUTDIR=${2:-.}
 OUTDIR=$(cd $OUTDIR && pwd)
 script_dir=$(cd $(dirname $BASH_SOURCE); pwd)
+export PIPENV_PIPFILE=${script_dir}/../Pipfile
 
 mkdir -p $OUTDIR
 cd $OUTDIR
@@ -12,13 +13,13 @@ cd $OUTDIR
 plotpy="${script_dir}/plot.py"
 
 # plot topological properties
-python $plotpy "${INDIR}/cc_degree_correlation.dat" --xlog --ylog --xlabel 'k' --ylabel 'C(k)' -f png
-python $plotpy "${INDIR}/degree_distribution.dat" --ylog --xlabel 'degree' --ylabel 'frequency' -f png
-python $plotpy "${INDIR}/neighbor_degree_correlation.dat" --xlog --xlabel 'k' --ylabel '$k_{nn}(k)$' -f png
+pipenv run python $plotpy "${INDIR}/cc_degree_correlation.dat" --xlog --ylog --xlabel 'k' --ylabel 'C(k)' -f png
+pipenv run python $plotpy "${INDIR}/degree_distribution.dat" --ylog --xlabel 'degree' --ylabel 'frequency' -f png
+pipenv run python $plotpy "${INDIR}/neighbor_degree_correlation.dat" --xlog --xlabel 'k' --ylabel '$k_{nn}(k)$' -f png
 
-python $plotpy "${INDIR}/num_communities.dat" --ylog --xlabel '# of communities' --ylabel 'frequency' -f png
-python $plotpy "${INDIR}/community_size.dat" --ylog --xlabel 'community size' --ylabel 'frequency' -f png
-python $plotpy "${INDIR}/num_egocom_histo.dat" --ylog --xlabel '# of communities' --ylabel 'frequency' -f png
+pipenv run python $plotpy "${INDIR}/num_communities.dat" --ylog --xlabel '# of communities' --ylabel 'frequency' -f png
+pipenv run python $plotpy "${INDIR}/community_size.dat" --ylog --xlabel 'community size' --ylabel 'frequency' -f png
+pipenv run python $plotpy "${INDIR}/num_egocom_histo.dat" --ylog --xlabel '# of communities' --ylabel 'frequency' -f png
 
 # plot weight-related properties
 if [ -e $INDIR/edge_weight_distribution.dat ]; then
@@ -28,13 +29,13 @@ ruby ${ensemble_average_rb} -l -f "${INDIR}/edge_weight_distribution.dat" -o "${
 ruby ${ensemble_average_rb} -l "${INDIR}/overlap_weight_correlation.dat" -o "${INDIR}/overlap_weight_correlation_logbin.dat"
 ruby ${ensemble_average_rb} -l -f "${INDIR}/strength_distribution.dat" -o "${INDIR}/strength_distribution_logbin.dat"
 
-python $plotpy "${INDIR}/edge_weight_distribution_logbin.dat" --xlog --ylog --xlabel '$w_{ij}$', --ylabel 'frequency' -f png
-python $plotpy "${INDIR}/overlap_weight_correlation_logbin.dat" --xlog --xlabel 'w' --ylabel 'O(w)' -f png
-python $plotpy "${INDIR}/strength_degree_correlation.dat" --xlog --ylog --xlabel 'k' --ylabel 's(k)' -f png
-python $plotpy "${INDIR}/strength_distribution_logbin.dat" --xlog --ylog --xlabel 's' --ylabel 'frequency' -f png
+pipenv run python $plotpy "${INDIR}/edge_weight_distribution_logbin.dat" --xlog --ylog --xlabel '$w_{ij}$', --ylabel 'frequency' -f png
+pipenv run python $plotpy "${INDIR}/overlap_weight_correlation_logbin.dat" --xlog --xlabel 'w' --ylabel 'O(w)' -f png
+pipenv run python $plotpy "${INDIR}/strength_degree_correlation.dat" --xlog --ylog --xlabel 'k' --ylabel 's(k)' -f png
+pipenv run python $plotpy "${INDIR}/strength_distribution_logbin.dat" --xlog --ylog --xlabel 's' --ylabel 'frequency' -f png
 
-python "${script_dir}/link_removal_percolation.py" "${INDIR}/link_removal_percolation.dat" "${OUTDIR}/link_removal_percolation.png"
-python "${script_dir}/link_removal_percolation_scale.py" "${INDIR}/link_removal_percolation.dat" "${OUTDIR}/link_removal_percolation_scale.png"
+pipenv run python "${script_dir}/link_removal_percolation.py" "${INDIR}/link_removal_percolation.dat" "${OUTDIR}/link_removal_percolation.png"
+pipenv run python "${script_dir}/link_removal_percolation_scale.py" "${INDIR}/link_removal_percolation.dat" "${OUTDIR}/link_removal_percolation_scale.png"
 
 fi
 
--- run.sh
+++ run.sh
@@ -10,11 +10,11 @@ mkdir -p $OUT_DIR
 cd $OUT_DIR
 $script_dir/analyzer.out $NET_FILE
 $script_dir/infomap/Infomap $NET_FILE . -i link-list -z -2 --overlapping --clu --out-name community
-python $script_dir/parse_clu.py community.clu > community_stat.json
-python $script_dir/egonet_community.py $NET_FILE > num_egocoms.json
+pipenv run python $script_dir/parse_clu.py community.clu > community_stat.json
+pipenv run python $script_dir/egonet_community.py $NET_FILE > num_egocoms.json
 $script_dir/egonet_com.out $NET_FILE > num_egocoms.json
 #python $script_dir/egonet_community.py $NET_FILE > num_egocoms.json
-python $script_dir/merge_json.py _output.json community_stat.json num_egocoms.json > merged.json
+pipenv run python $script_dir/merge_json.py _output.json community_stat.json num_egocoms.json > merged.json
 mv merged.json _output.json 
 rm community_stat.json num_egocoms.json
 
